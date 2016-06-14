aa96574911b563b82ed2acf204fb9f169f48e98c
samridhgupta <samridhgupta@gmail.com>
2016-06-15 04:52:29 +0530

    ...

---
+++ app/src/main/java/game.java
@@ -0,0 +1,27 @@
+/**
+ * Created by Samridh Gupta on 6/15/2016.
+ *//*
+
+
+public class game {
+
+    public String user1;
+    public String user2;
+
+}
+
+public class User {
+
+    public String username;
+    public String email;
+
+    public User() {
+        // Default constructor required for calls to DataSnapshot.getValue(User.class)
+    }
+
+    public User(String username, String email) {
+        this.username = username;
+        this.email = email;
+    }
+
+}*/
